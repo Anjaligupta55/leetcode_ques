084ba251cf5be4ee73310da323a8d5cb63f75996
Anjali gupta <anjaligupta225196@gmail.com>
2026-09-12 00:33:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int solve(int i,int n,vector<int>&dp){
-        if( i==n){
+    int solve(int i,vector<int>&dp){
+        if(i==1 || i==0){
             return 1;
-        }
-        if(i>n){
-            return 0;
         }
         if(dp[i]!=-1){
             return dp[i];
         }
-        return dp[i]=solve(i+1,n,dp)+solve(i+2,n,dp);
+        return dp[i]=solve(i-1,dp)+solve(i-2,dp);
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-       
-        return solve(0,n,dp);
+        int ans=solve(n,dp);
+        return ans;
     }
 };
