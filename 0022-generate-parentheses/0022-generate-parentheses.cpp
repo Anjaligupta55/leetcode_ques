b192dc4173a8ea85349d88ed91474e0a0ff6708c
Anjali gupta <anjaligupta225196@gmail.com>
2026-09-12 22:50:26 +0530

    Time: 5 ms (15.82%), Space: 16.3 MB (15.08%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void solve(int n,int l,int r,vector<string>&ans,string s){
+    void solve(int n,int l,int r,vector<string>& ans,string s){
+       
         if(s.size()==2*n){
             ans.push_back(s);
             return;
         }
+           
         if(l<n){
-            s+='(';
+            s+="(";
             solve(n,l+1,r,ans,s);
             s.pop_back();
         }
         if(r<l){
-            s+=')';
+            s+=")";
             solve(n,l,r+1,ans,s);
             s.pop_back();
+            
         }
+        
     }
     vector<string> generateParenthesis(int n) {
-
-        string s="";
         vector<string>ans;
-        solve(n,0,0,ans,s);
+        solve(n,0,0,ans,"");
         return ans;
-        
-        
     }
 };
